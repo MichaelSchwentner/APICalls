816c8ddb0f64922a0e6ca9ef1355c0f9519662a3
michaelschwentner <mid.schwentner@gmail.com>
2023-02-10 12:09:42 +0100

    Update

--- app/src/main/java/com/example/apicalls/MainViewModel.kt
+++ app/src/main/java/com/example/apicalls/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.apicalls
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.apicalls.data.AppRepository
@@ -9,10 +11,18 @@ import kotlinx.coroutines.launch
 
 const val TAG = "MainViewModel"
 
+enum class Weekday {MO, DIE, MI, DO, FR, SA, SO}
+
+enum class ApiStatus { LOADING, ERROR, DONE }
+
 class MainViewModel : ViewModel() {
     private val repository = AppRepository(GotApi)
 
     val characters = repository.characters
+
+    private val _loading = MutableLiveData<ApiStatus>()
+    val loading: LiveData<ApiStatus>
+    get() = _loading
 
     init {
         loadCharacters()
@@ -21,9 +31,12 @@ class MainViewModel : ViewModel() {
     private fun  loadCharacters() {
         viewModelScope.launch {
             try {
+                _loading.value = ApiStatus.LOADING
                 repository.loadCharacters()
+                _loading.value = ApiStatus.DONE
             } catch (e: Exception) {
                 Log.e("MainViewModel", "error loading charackters $e")
+                _loading.value = ApiStatus.ERROR
             }
         }
     }
